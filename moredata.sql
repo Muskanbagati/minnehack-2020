5346d2bbfc08f00cdfc50c1aff28f13fdd3162d8
BlueRobin999 <46752017+BlueRobin999@users.noreply.github.com>
2020-01-26 06:22:41 -0600

    Add files via upload

---
+++ moredata.sql
@@ -0,0 +1,35 @@
+Insert into product
+values (9, "https://us.davines.com/products/oi-conditioner", 40, 180);
+
+Insert into product_cat
+values (9, 0, 1, 0, 0, 0);
+
+Insert into product_cause
+values (9, 1, 1, 0, 0, 0, 0, 0);
+
+Insert into product
+values (10, "https://packagefreeshop.com/collections/featured-products/products/im-not-trash-sweatshirt", 55, 20);
+
+Insert into product_cat
+values (10, 1, 0, 0, 0, 0);
+
+Insert into product_cause
+values (10, 0, 0, 1, 1, 0, 0, 1);
+
+Insert into product
+values (9, "https://us.davines.com/products/oi-conditioner", 40, 180);
+
+Insert into product_cat
+values (9, 0, 1, 0, 0, 0);
+
+Insert into product_cause
+values (9, 1, 1, 0, 0, 0, 0, 0);
+
+Insert into product
+values (10, "https://packagefreeshop.com/collections/featured-products/products/im-not-trash-sweatshirt", 55, 20);
+
+Insert into product_cat
+values (10, 1, 0, 0, 0, 0);
+
+Insert into product_cause
+values (10, 0, 0, 1, 1, 0, 0, 1);
